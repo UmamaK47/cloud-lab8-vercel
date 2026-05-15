183ef585ab89d48ffee73a5c1ef733b6cb9fb762
Umama Khan <umamak847@gmail.com>
2026-05-15 21:11:01 +0500

    Fixed student dynamic route

--- app/api/student/107/route.ts
+++ app/api/student/107/route.ts
@@ -1,21 +1,23 @@
 import { NextResponse } from "next/server";
 
 const data: Record<string, object> = {
-  "001": { name: "Ali Ahmed", lab: 8 },
-  "002": { name: "Sara Khan", lab: 8 },
+  "107": { name: "Umama Khan", lab: 8 },
 };
 
 export async function GET(
-  _: Request,
-  { params }: { params: { id: string } }
+  request: Request,
+  context: { params: Promise<{ id: string }> }
 ) {
+  const params = await context.params;
+
   const student = data[params.id];
 
-  if (!student)
+  if (!student) {
     return NextResponse.json(
       { error: "Not found" },
       { status: 404 }
     );
+  }
 
   return NextResponse.json(student);
 }
